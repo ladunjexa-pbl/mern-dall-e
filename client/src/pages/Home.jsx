94eef4578d9ce044243885d6f59589ef95eca1f3
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-01-21 12:39:39 +0200

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://dall-e-fpb3.onrender.com/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
